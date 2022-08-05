7add2f67114bf5c4cda8aba8ac5c3660e904daa6
Johann Kellerman <kellerza@gmail.com>
2022-08-05 09:18:33 +0000

    temp

--- helpers/ws_template.go
+++ helpers/ws_template.go
@@ -10,13 +10,15 @@ import (
 	gomData "github.com/hairyhenderson/gomplate/v3/data"
 	jT "github.com/kellerza/template"
 	log "github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v3"
 )
 
 type WebSocketTemplate struct {
-	Name     string                 `json:"name,omitempty"`
-	Template string                 `json:"template,omitempty"`
-	Vars     map[string]interface{} `json:"vars,omitempty"`
-	Result   string                 `json:"result,omitempty"`
+	Name       string                 `json:"name,omitempty"`
+	Template   string                 `json:"template,omitempty"`
+	Vars       map[string]interface{} `json:"vars,omitempty"`
+	Result     string                 `json:"result,omitempty"`
+	ResultYaml map[string]interface{} `json:"resulty,omitempty"`
 }
 
 func NewTemplate() *template.Template {
@@ -67,6 +69,15 @@ func (t *WebSocketTemplate) Render(ctx *Context) error {
 		return err
 	}
 	t.Result = buf.String()
+
+	// try parse to object
+	t.ResultYaml = make(map[string]interface{})
+	err = yaml.Unmarshal([]byte(t.Result), t.ResultYaml)
+	if err != nil {
+		log.Errorf("%s", t.Result)
+	}
+
 	log.Infof("Rendered: %s", t.Result)
+
 	return nil
 }
