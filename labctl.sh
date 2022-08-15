5b4ce4bc7a82ab3cfa467efbee05323a5dbbb75f
Johann Kellerman <kellerza@gmail.com>
2022-08-15 08:08:41 +0000

    releaser

--- labctl.sh
+++ labctl.sh
@@ -1,4 +1,10 @@
 #/bin/bash
+#
+# Compile and run labctl
+#
+# Add a labctl alias in your shell. E.g. in ~/.zshrc add the following:
+#  alias labctl='/home/kellerza/go/src/github.com/labctl/labctl/labctl.sh'
+#
 set -e
 pwd=$PWD
 src=${0%/*}
